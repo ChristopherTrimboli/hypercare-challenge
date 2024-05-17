aaa51dcf84443f7a5a7521a038a8f02727d86c3c
Christopher Trimboli <cjft@trimboli.io>
2024-05-17 09:53:51 -0600

    nextjs init, repo setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hypercare-challenge
 Hypercare Frontend Developer Coding Challenge
+
+`npm i`
+
+`npm run dev`
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Hypercare</h1>
+      <p>View information about Hypercare users.</p>
+    </main>
+  );
+}
